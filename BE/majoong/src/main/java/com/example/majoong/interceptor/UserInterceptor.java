6f15be6343c70f1fd9eeec30a06dcbcb5728495d
wlsk0227 <wlsk0227@daum.net>
2023-04-26 10:18:47 +0900

    [FEAT] jwt prehandler

---
+++ BE/majoong/src/main/java/com/example/majoong/interceptor/UserInterceptor.java
@@ -0,0 +1,29 @@
+package com.example.majoong.interceptor;
+
+import com.example.majoong.tools.JwtTool;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class UserInterceptor implements HandlerInterceptor {
+
+    private final JwtTool jwtTool;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String token = request.getHeader("Authorization");
+
+        if( token == null || token.split(" ").length != 2 || !jwtTool.validateToken(token)) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED,"JWT 만료");
+            return false;
+        }
+        return true;
+    }
+}
