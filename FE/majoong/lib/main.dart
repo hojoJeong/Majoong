8b0e92c966f0042cdfe603fe34cfd76806b8135b
Suyong <suyong5713@gmail.com>
2023-04-27 13:47:13 +0900

    [CHORE] @suyong5713 - 패키지 이름 수정

--- FE/majoong/lib/main.dart
+++ FE/majoong/lib/main.dart
@@ -2,16 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:majoong/view/splash_screen.dart';
 
 void main() {
-  runApp(const Majoong());
-}
-
-class Majoong extends StatelessWidget {
-  const Majoong({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: SplashScreen(),
-    );
-  }
+  runApp(const MaterialApp(
+    home: SplashScreen(),
+  ));
 }
