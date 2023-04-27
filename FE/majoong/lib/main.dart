dde6716da262f44d1639034c604b092d4c820ed6
Suyong <suyong5713@gmail.com>
2023-04-27 14:06:30 +0900

    [ADD] @suyong5713 - retrofit, kakaoSDK, riverpod 패키지 추가

--- FE/majoong/lib/main.dart
+++ FE/majoong/lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:majoong/view/splash_screen.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: SplashScreen(),
-  ));
+  runApp(
+    const MaterialApp(
+      home: SplashScreen(),
+    ),
+  );
 }
