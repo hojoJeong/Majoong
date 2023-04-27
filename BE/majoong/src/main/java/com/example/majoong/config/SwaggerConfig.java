307ecdb3113e2e4e59b2c511ca4e8fbb09d7b249
wlsk0227 <wlsk0227@daum.net>
2023-04-27 17:53:24 +0900

    [CHORE] @wlsk0227 swagger 설정

--- BE/majoong/src/main/java/com/example/majoong/config/SwaggerConfig.java
+++ BE/majoong/src/main/java/com/example/majoong/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package com.example.majoong.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,15 +12,31 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
 
+
     @Bean
     public OpenAPI openAPI() {
         Info info = new Info()
                 .title("마중 API")
                 .version("v1.0.0")
                 .description("마중 프로젝트 API 명세서입니다.");
+
+        String jwtSchemeName = "jwtAuth";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
+                        .name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("Bearer")
+                        .bearerFormat("JWT"));
+
+
         return new OpenAPI()
-                .components(new Components())
-                .info(info);
+                .components(components)
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                ;
+
+
     }
 }
 
