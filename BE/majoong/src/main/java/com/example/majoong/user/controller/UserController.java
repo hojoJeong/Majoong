40bdd0b4d68394d31df7c77820a821797f9e6818
wlsk0227 <wlsk0227@daum.net>
2023-04-28 09:33:07 +0900

    [FIX] @wlsk0227 휴대폰번호 인증 path 수정

--- BE/majoong/src/main/java/com/example/majoong/user/controller/UserController.java
+++ BE/majoong/src/main/java/com/example/majoong/user/controller/UserController.java
@@ -66,21 +66,15 @@ public class UserController {
         return data.builder();
     }
 
-    @GetMapping("/test")
-    public ResponseEntity test() {
-        ResponseData data = new ResponseData();
-        data.setData("하잉~");
-        return data.builder();
-    }
 
-    @PostMapping("/auth")
+    @PostMapping("/phone")
     public ResponseEntity<?> sendAuthNumber(@RequestBody PhoneNumberDto info) throws NoSuchAlgorithmException, URISyntaxException, InvalidKeyException, JsonProcessingException, UnsupportedEncodingException {
         ResponseData data = new ResponseData();
         data.setData(messageService.sendMessage(info.getPhoneNumber()));
         return data.builder();
     }
 
-    @PostMapping("/auth/verify")
+    @PostMapping("/phone/verify")
     public ResponseEntity<?> verifyAuthNumber(@RequestBody VerificationNumberDto info) {
         ResponseData data = new ResponseData();
         if (messageService.verifyNumber(info)){
