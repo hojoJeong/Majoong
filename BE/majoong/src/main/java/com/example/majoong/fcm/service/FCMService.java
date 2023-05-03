b9bf762d60340262ca0634b88e41c6d034b8c86b
wlsk0227 <wlsk0227@daum.net>
2023-05-03 15:34:18 +0900

    [FEAT] @wlsk0227 fcm setting

---
+++ BE/majoong/src/main/java/com/example/majoong/fcm/service/FCMService.java
@@ -0,0 +1,86 @@
+package com.example.majoong.fcm.service;
+
+import com.example.majoong.fcm.dto.FCMMessageDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.auth.oauth2.GoogleCredentials;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Service;
+import java.io.IOException;
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
+public class FCMService {
+
+    private final ObjectMapper objectMapper;
+
+    @Value("${FCM_API_URL}")
+    private String API_URL;
+    public String getAccessToken() throws IOException {
+
+        GoogleCredentials googleCredentials = GoogleCredentials
+                .fromStream(new ClassPathResource("firebase_service_key.json").getInputStream())
+                .createScoped(List.of("https://www.googleapis.com/auth/cloud-platform"));
+
+        googleCredentials.refreshIfExpired();
+        return googleCredentials.getAccessToken().getTokenValue();
+    }
+
+
+    public String makeMessage(
+            String targetToken, String title, String body, String name, String description
+    ) throws JsonProcessingException {
+        FCMMessageDto fcmMessage = FCMMessageDto.builder()
+                .message(
+                        FCMMessageDto.Message.builder()
+                                .token(targetToken)
+                                .notification(
+                                        FCMMessageDto.Notification.builder()
+                                                .title(title)
+                                                .body(body)
+                                                .build()
+                                )
+                                .data(
+                                        FCMMessageDto.Data.builder()
+                                                .name(name)
+                                                .description(description)
+                                                .build()
+                                )
+                                .build()
+                )
+                .validateOnly(false)
+                .build();
+
+        return objectMapper.writeValueAsString(fcmMessage);
+
+    }
+    public void sendMessage(
+            String targetToken, String title, String body, String name, String description
+    ) throws IOException{
+
+        String message = makeMessage(targetToken, title, body, name, description);
+
+        OkHttpClient client = new OkHttpClient();
+        RequestBody requestBody = RequestBody.create(message, MediaType.get("application/json; charset=utf-8"));
+
+        Request request = new Request.Builder()
+                .url(API_URL)
+                .post(requestBody)
+                .addHeader(HttpHeaders.AUTHORIZATION, "Bearer "+ getAccessToken())
+                .addHeader(HttpHeaders.CONTENT_TYPE, "application/json; UTF-8")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        log.info(response.body().string());
+
+    }
+
+
+}
