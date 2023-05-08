d0e14e894435c320996c3536d39fb060128fffa7
hojo0423 <hojo0423@naver.com>
2023-05-08 22:06:18 +0900

    [FEAT] @hojo0423 -firebase 연동

--- FE/majoong/lib/main.dart
+++ FE/majoong/lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'package:logger/logger.dart';
 import 'package:majoong/common/const/app_key.dart';
+import 'package:majoong/common/util/logger.dart';
+import 'package:majoong/firebase_options.dart';
 import 'package:majoong/view/login/login_screen.dart';
 import 'package:majoong/view/login/splash_screen.dart';
 import 'package:majoong/common/util/provider_logger.dart';
@@ -11,6 +15,7 @@ import 'package:majoong/common/util/provider_logger.dart';
 void main() {
   KakaoSdk.init(nativeAppKey: KAKAO_NATIVE_KEY);
   GetStorage.init();
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     ProviderScope(
       observers: [ProviderLogger()],
