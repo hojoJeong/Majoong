9346f1623a7940a58e60b0c9cfdc68b9abacf2a9
hojo0423 <hojo0423@naver.com>
2023-05-08 23:49:44 +0900

    [FIX] @hojo0423 - 자동로그인 누락 오류 수정

--- FE/majoong/lib/view/login/splash_screen.dart
+++ FE/majoong/lib/view/login/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:majoong/common/util/logger.dart';
 import 'package:majoong/model/response/base_response.dart';
 import 'package:majoong/view/main/main_screen.dart';
 import 'package:majoong/view/login/login_screen.dart';
@@ -12,17 +13,19 @@ class SplashScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final checkAutoLoginState = ref.watch(checkAutoLoginProvider);
+    final checkAutoLoginState = ref.read(checkAutoLoginProvider);
     final autoLoginState = ref.watch(loginProvider);
-    if (checkAutoLoginState) {
+    if (checkAutoLoginState == 1) {
+      logger.d('자동로그인 맞음!');
       ref.read(loginProvider.notifier).login(null);
       if (autoLoginState is BaseResponse && autoLoginState.status == 200) {
-        Future.delayed(Duration.zero, () {
+        Future.delayed(Duration(seconds: 2), () {
           Navigator.pushReplacement(
               context, MaterialPageRoute(builder: (context) => MainScreen()));
         });
       }
-    } else {
+    } else if(checkAutoLoginState == 0) {
+      logger.d('자동로그인 아님!');
       Future.delayed(Duration(seconds: 2), () {
         Navigator.pushReplacement(context,
             MaterialPageRoute(builder: (context) => const LoginScreen()));
